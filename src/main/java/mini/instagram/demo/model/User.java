97e036244616c8e7392cdc2e65950d9c802d7352
Nguyễn Tiến Đạt <112878795+NguyenTienDat377@users.noreply.github.com>
2024-08-30 20:29:13 +0700

    First change

---
+++ src/main/java/mini/instagram/demo/model/User.java
@@ -0,0 +1,10 @@
+package mini.instagram.demo.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+    private int followers = 0;
+    private int followingUser = 0;
+    
+}
