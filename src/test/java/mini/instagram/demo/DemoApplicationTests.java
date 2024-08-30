28dd99af7b69534b827836521434abb5d94f2e3d
Nguyễn Tiến Đạt <112878795+NguyenTienDat377@users.noreply.github.com>
2024-08-30 20:29:13 +0700

    First change

---
+++ src/test/java/mini/instagram/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mini.instagram.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
